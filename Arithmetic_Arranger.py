89689f52aeb5bde27e73c4f1632f9688e05bb307
PhumeleleTom <74917789+PhumeleleTom@users.noreply.github.com>
2022-09-08 11:53:57 +0200

    Update Arithmetic_Arranger.py
    Line 70: Removed addition fo spaces, replaced with dashes, "-".
    Line 76: Removed dashes, replaced with spaces, " ".

--- Arithmetic_Arranger.py
+++ Arithmetic_Arranger.py
@@ -67,13 +67,13 @@ def arithmetic_arranger(myList, printAnswer=False):
         
         outputLine_1 += " "+" "+" "*(bigNumLen-myStringLen_0)+myString[0]+" "*4
         outputLine_2 += myString[1]+" "+" "*(bigNumLen-myStringLen_2)+myString[2]+" "*4
-        outputLine_3 += " "+" "+"-"*(bigNumLen)+" "*4
+        outputLine_3 += "-"+"-"+"-"*(bigNumLen)+" "*4
         if(printAnswer):
             if(myString[1]=='+'):
                 answer = int(myString[0])+int(myString[2])
             else:
                 answer = int(myString[0])-int(myString[2])
-            outputLine_4 += "-"+"-"*(bigNumLen-len(str(answer)))+str(answer)+" "*4
+            outputLine_4 += " "+" "*(bigNumLen-len(str(answer)))+str(answer)+" "*4
             
     output = outputLine_1 + "\n" + outputLine_2 + "\n" + outputLine_3 + "\n" + outputLine_4
     return output

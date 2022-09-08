54338de4c1184eaa96b32552ad0edc2853ee06b2
PhumeleleTom <74917789+PhumeleleTom@users.noreply.github.com>
2022-09-08 11:46:38 +0200

    Update Arithmetic_Arranger.py
    Removed numDiffLen and totalLen, replaced with bigNumLen. bignumLen is used in the appropriate places in the outputLines in lines 68,69,70 and 76.

--- Arithmetic_Arranger.py
+++ Arithmetic_Arranger.py
@@ -62,17 +62,19 @@ def arithmetic_arranger(myList, printAnswer=False):
         else:
             bigNumPos = 2
             smallNumPos = 0
-        numDiffLen = len(myString[bigNumPos]) - len(myString[smallNumPos])
-        totalLen = len(myString[bigNumPos]) + len(myString[smallNumPos])
-        outputLine_1 += " "*(totalLen-myStringLen_0)+myString[0]+" "*4
-        outputLine_2 += myString[1]+" "*(totalLen-myStringLen_2-1)+myString[2]+" "*4
-        outputLine_3 += "-"*(totalLen)+" "*4
+            
+        bigNumLen = len(myString[bigNumPos])
+        
+        outputLine_1 += " "+" "+" "*(bigNumLen-myStringLen_0)+myString[0]+" "*4
+        outputLine_2 += myString[1]+" "+" "*(bigNumLen-myStringLen_2)+myString[2]+" "*4
+        outputLine_3 += " "+" "+"-"*(bigNumLen)+" "*4
         if(printAnswer):
             if(myString[1]=='+'):
                 answer = int(myString[0])+int(myString[2])
             else:
                 answer = int(myString[0])-int(myString[2])
-            outputLine_4 += " "*((totalLen)-len(str(answer)))+str(answer)+" "*4
+            outputLine_4 += " "+" "*(bigNumLen-len(str(answer)))+str(answer)+" "*4
+            
     output = outputLine_1 + "\n" + outputLine_2 + "\n" + outputLine_3 + "\n" + outputLine_4
     return output
 
